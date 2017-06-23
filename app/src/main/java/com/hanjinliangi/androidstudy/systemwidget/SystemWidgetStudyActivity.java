bdd9e4a5a36cd6da1b9cad3b075d0aad861519c2
HanJinLiang <hanjinliang1992@163.com>
2017-06-23 18:16:23 +0800

    Initial commit

---
+++ app/src/main/java/com/hanjinliangi/androidstudy/systemwidget/SystemWidgetStudyActivity.java
@@ -0,0 +1,18 @@
+package com.hanjinliangi.androidstudy.systemwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hanjinliangi.androidstudy.R;
+
+/**
+ * 系统组件
+ */
+public class SystemWidgetStudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_widget_study);
+    }
+}
