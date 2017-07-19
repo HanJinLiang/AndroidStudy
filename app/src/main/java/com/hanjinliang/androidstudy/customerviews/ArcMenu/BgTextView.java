ecc6618ec03d37b4f816c19d6341ea88aa1ca591
HanJinLiang <hanjinliang1992@163.com>
2017-07-19 18:21:59 +0800

    加强自定义背景View

--- app/src/main/java/com/hanjinliang/androidstudy/customerviews/ArcMenu/BgTextView.java
+++ app/src/main/java/com/hanjinliang/androidstudy/customerviews/ArcMenu/BgTextView.java
@@ -6,9 +6,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 
 import com.hanjinliang.androidstudy.R;
 
@@ -19,8 +21,22 @@ public class BgTextView extends android.support.v7.widget.AppCompatTextView {
     Path mPath;
     Paint mPaint;
     private int mWidth,mHeight;
-    private float mStrokeWidth=10;
+    private float mStrokeWidth ;
     private int mBgColor;
+
+    private float mLeftTopRadius ;
+    private float mLeftBottomRadius ;
+    private float mRightTopRadius ;
+    private float mRightBottomRadius ;
+    private float mRadius ;
+
+
+    //控制4个边是否绘制
+    private boolean isDrawRight=false;
+    private boolean isDrawLeft=true;
+    private boolean isDrawBottom=true;
+    private boolean isDrawTop=true;
+
     public BgTextView(Context context) {
         this(context,null);
     }
@@ -34,8 +50,25 @@ public class BgTextView extends android.support.v7.widget.AppCompatTextView {
 
         //获取自定义属性
         TypedArray ta=context.obtainStyledAttributes(attrs, R.styleable.BgTextView,defStyleAttr,0);
-        mStrokeWidth=ta.getDimension(R.styleable.BgTextView_BgStrokeWidth,12);
+        mStrokeWidth=ta.getDimension(R.styleable.BgTextView_BgStrokeWidth,TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,1,getResources().getDisplayMetrics()));
         mBgColor=ta.getColor(R.styleable.BgTextView_BgColor,Color.BLUE);
+
+        mLeftTopRadius=ta.getDimension(R.styleable.BgTextView_LeftTopRadius,mStrokeWidth);
+        mLeftBottomRadius=ta.getDimension(R.styleable.BgTextView_LeftBottomRadius,mStrokeWidth);
+        mRightBottomRadius=ta.getDimension(R.styleable.BgTextView_RightBottomRadius,mStrokeWidth);
+        mRightTopRadius=ta.getDimension(R.styleable.BgTextView_RightTopRadius,mStrokeWidth);
+        mRadius=ta.getDimension(R.styleable.BgTextView_Radius,-1);
+        if(mRadius!=-1){//有设置mRadius
+            mLeftTopRadius=mRadius;
+            mLeftBottomRadius=mRadius;
+            mRightBottomRadius=mRadius;
+            mRightTopRadius=mRadius;
+        }
+        isDrawRight=ta.getBoolean(R.styleable.BgTextView_DrawRight,true);
+        isDrawLeft=ta.getBoolean(R.styleable.BgTextView_DrawLeft,true);
+        isDrawBottom=ta.getBoolean(R.styleable.BgTextView_DrawBottom,true);
+        isDrawTop=ta.getBoolean(R.styleable.BgTextView_DrawTop,true);
+
         //记得回收
         ta.recycle();
 
@@ -47,26 +80,140 @@ public class BgTextView extends android.support.v7.widget.AppCompatTextView {
         mPath=new Path();
     }
 
+
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth=w;
         mHeight=h;
-        //绘制背景Path
-        mPath.reset();
-        mPath.moveTo(mWidth,mHeight-mStrokeWidth/2);
-        mPath.lineTo(mHeight/2,mHeight-mStrokeWidth/2);
-        mPath.addArc(new RectF(mStrokeWidth/2,mStrokeWidth/2,mHeight-mStrokeWidth/2,mHeight-mStrokeWidth/2),90,180);
-        mPath.lineTo(mWidth,mStrokeWidth/2);
-        //设置padding 保证右边圆环没有文字
-        setPadding(mHeight/2,0,0,0);
+        initPath();
     }
 
+    /**
+     * 初始化path
+     */
+    private void initPath() {
+        controlRadius();
+
+        //绘制背景Path
+        mPath.reset();
+        //移动到右上角
+        mPath.moveTo(mWidth-mRightTopRadius,0+mStrokeWidth/2);
+        if(isDrawRight) {//需要画右边
+            mPath.addArc(new RectF(mWidth - mRightTopRadius * 2 + mStrokeWidth / 2, 0 + mStrokeWidth / 2, mWidth - mStrokeWidth / 2, mRightTopRadius * 2 - mStrokeWidth / 2), -90, 90);
+            mPath.lineTo(mWidth - mStrokeWidth / 2, mHeight - mRightBottomRadius);
+            mPath.addArc(new RectF(mWidth - mRightBottomRadius * 2 + mStrokeWidth / 2, mHeight - mRightBottomRadius * 2 + mStrokeWidth / 2, mWidth - mStrokeWidth / 2, mHeight - mStrokeWidth / 2), 0, 90);
+        }else{//不需要直接移动到
+            mPath.moveTo(mWidth-mRightBottomRadius, mHeight - mStrokeWidth / 2);
+        }
+
+
+        if(isDrawBottom) {//连接到左下角
+            mPath.lineTo(mLeftBottomRadius, mHeight - mStrokeWidth / 2);
+        }else{//移动到左下角
+            mPath.moveTo(mLeftBottomRadius, mHeight - mStrokeWidth / 2);
+        }
+
+        if(isDrawLeft) {//需要画左边
+            mPath.addArc(new RectF(0 + mStrokeWidth / 2, mHeight - mLeftBottomRadius * 2 + mStrokeWidth / 2, mLeftBottomRadius * 2 - mStrokeWidth / 2, mHeight - mStrokeWidth / 2), 90, 90);
+
+            mPath.lineTo(0 + mStrokeWidth / 2, mLeftTopRadius);
+            mPath.addArc(new RectF(0 + mStrokeWidth / 2, 0 + mStrokeWidth / 2, mLeftTopRadius * 2 - mStrokeWidth / 2, mLeftTopRadius * 2 - mStrokeWidth / 2), 180, 90);
+        }else{//直接移动到
+            mPath.moveTo(0+mLeftBottomRadius, 0 + mStrokeWidth / 2);
+        }
+
+        //连接到左上角
+        if(isDrawTop) {
+            mPath.lineTo(mWidth-mRightTopRadius,0+mStrokeWidth/2);
+        }else{
+            mPath.moveTo(mWidth-mRightTopRadius,0+mStrokeWidth/2);
+        }
+    }
+
+    /**
+     * 控制各个Radius 在[mStrokeWidth,mHeight/2]区间
+     */
+    private void controlRadius() {
+        if(mLeftTopRadius<mStrokeWidth){
+            mLeftTopRadius=mStrokeWidth;
+        }
+        if(mLeftBottomRadius<mStrokeWidth){
+            mLeftBottomRadius=mStrokeWidth;
+        }
+        if(mRightTopRadius<mStrokeWidth){
+            mRightTopRadius=mStrokeWidth;
+        }
+        if(mRightBottomRadius<mStrokeWidth){
+            mRightBottomRadius=mStrokeWidth;
+        }
+
+        if(mLeftTopRadius>mHeight/2){
+            mLeftTopRadius=mHeight/2;
+        }
+        if(mLeftBottomRadius>mHeight/2){
+            mLeftBottomRadius=mHeight/2;
+        }
+        if(mRightTopRadius>mHeight/2){
+            mRightTopRadius=mHeight/2;
+        }
+        if(mRightBottomRadius>mHeight/2){
+            mRightBottomRadius=mHeight/2;
+        }
+    }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.GRAY);
         canvas.drawPath(mPath,mPaint);
         super.onDraw(canvas);
+    }
+
+    /**
+     * 设置四个边是否绘制
+     * @param isDrawRight
+     * @param isDrawLeft
+     * @param isDrawBottom
+     * @param isDrawTop
+     */
+    private void setDrawEnable(boolean isDrawRight,boolean isDrawLeft,boolean isDrawBottom,boolean isDrawTop){
+        this.isDrawRight=isDrawRight;
+        this.isDrawLeft=isDrawLeft;
+        this.isDrawBottom=isDrawBottom;
+        this.isDrawTop=isDrawTop;
+
+        initPath();
+        //刷新
+        invalidate();
+    }
+
+
+    /**
+     * 统一圆角
+     */
+    private void setRadius(float radius){
+        mRadius=radius;
+        mLeftTopRadius=mRadius;
+        mLeftBottomRadius=mRadius;
+        mRightBottomRadius=mRadius;
+        mRightTopRadius=mRadius;
+        initPath();
+        //刷新
+        invalidate();
+    }
+
+    /**
+     * 分别设置四个圆角
+     */
+    private void setRadius(float leftTopRadius,float leftBottomRadius,float rightBottomRadius,float rightTopRadius){
+        mLeftTopRadius=leftTopRadius;
+        mLeftBottomRadius=leftBottomRadius;
+        mRightBottomRadius=rightBottomRadius;
+        mRightTopRadius=rightTopRadius;
+        initPath();
+        //刷新
+        invalidate();
     }
 }
